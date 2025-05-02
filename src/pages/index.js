1165f9d880ab36be3e5d91a9ba90fa4305960ffa
Faisal Almasri <falmasri.ai@gmail.com>
2025-05-02 22:38:32 +0300

    fix profile pic for ipad

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
       >
         <Layout className="!pt-0 md:!pt-16 sm:!pt-16">
           <div className="flex w-full items-start justify-between md:flex-col">
-            <div className="w-1/2 lg:hidden md:inline-block md:w-full">
+            <div className="w-1/2 lg md:inline-block md:w-full">
               <Image
                 src={profilePic}
                 alt="Faisal Almasri"
